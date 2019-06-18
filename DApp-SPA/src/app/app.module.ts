6db03d79204d4bd7bd96ccfa9ab0ea2ea318cece
Livan Hagi Osman <lhagiosman@amiltone.com>
2019-06-18 17:54:57 +0200

    End of Section 5

--- DApp-SPA/src/app/app.module.ts
+++ DApp-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
@@ -16,7 +17,7 @@ import { RegisterComponent } from './register/register.component';
     AngularFontAwesomeModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ErrorInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
