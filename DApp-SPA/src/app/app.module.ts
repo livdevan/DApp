e47f2ab05b3dbc485e74461a55237ab24284f752
Livan Hagi Osman <lhagiosman@amiltone.com>
2019-06-18 14:54:15 +0200

    End of Section 4

--- DApp-SPA/src/app/app.module.ts
+++ DApp-SPA/src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule, AngularFontAwesomeModule],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AngularFontAwesomeModule,
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
