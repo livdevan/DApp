79960b72acd735f50b2247818e0930570159eb31
Livan Hagi Osman <lhagiosman@amiltone.com>
2019-06-19 13:53:05 +0200

    End of Section 6

--- DApp-SPA/src/app/app.module.ts
+++ DApp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
@@ -15,7 +16,8 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
     BrowserModule,
     HttpClientModule,
     AngularFontAwesomeModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [ErrorInterceptorProvider],
   bootstrap: [AppComponent]
